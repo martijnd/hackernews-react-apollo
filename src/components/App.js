0ef30188615c7495ec0566b29db76439028ce307
Martijn Dorsman <martijn.dorsman@gmail.com>
2020-12-23 23:27:42 +0100

    Complete tutorial until 'Pagination'

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+
+import Login from './Login';
+import Search from './Search';
+
+const App = () => {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route
+            exact
+            path="/create"
+            component={CreateLink}
+          />
+          <Route
+            exact
+            path="/create"
+            component={CreateLink}
+          />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default App;
